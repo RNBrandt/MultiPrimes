3cade78aa2c83d6571724e32786cb4f2cf506752
Reuben Brandt <Reubennbrandt@gmail.com>
2017-10-15 17:36:05 -0700

    Minor changes to syntax and method names. Create runner file. Add defaults and exceptions in initialize

--- prime_multiplication_table.rb
+++ prime_multiplication_table.rb
@@ -1,12 +1,13 @@
 class PrimeMultiplicationTable
   attr_reader :table_size
 
-  def initialize(table_size)
+  def initialize(table_size= 10)
+    raise 'The table size must be a positive integer' if table_size < 1
     @table_size = table_size
   end
 
   def is_prime?(number)
-    return true if (number == 2) || (number == 3)
+    return true if number == 2
     return false if number_is_clearly_non_prime(number)
     compare_factors(number)
   end
@@ -15,10 +16,28 @@ class PrimeMultiplicationTable
     @primes ||= set_primes
   end
 
+  def prime_table_printer
+    print "\n"
+    print " " * 6
+    primes.each { |prime| print "%-6d" % prime }
+    print "\n"
+    primes.length.times { print '-------' }
+    print "\n"
+
+    prime_multiple_table_rows.each do |table_row|
+      table_row.each { |cell_value| print "%-6d" % cell_value }
+      print "\n"
+    end
+  end
+
+   def prime_multiple_table_rows
+    @primes_multiples ||= prime_multiple_row_creator
+  end
+
   private
 
-  def primes_multiples
-    primes.map { |x_prime| primes.map { |y_prime| x_prime * y_prime} }
+  def prime_multiple_row_creator
+    primes.map { |x_prime| [x_prime] + primes.map { |y_prime| x_prime * y_prime} }
   end
 
   def set_primes
@@ -36,22 +55,11 @@ class PrimeMultiplicationTable
   end
 
   def compare_factors(number)
-    #cribbing from the Sieve of Eratosthenes algorithm
+    #cribbed from the Sieve of Eratosthenes algorithm
     root = Math.sqrt(number)
-    return false if whole_number?(root)
-    factor = 3
-    until factor == root
-      if number % factor == 0
-        factor += 2
-        return false
-      else
-        return true
-      end
+    2.upto(root.to_i) do |sieve|
+     return false if number % sieve == 0
     end
+    return true
   end
-
-  def whole_number?(root)
-    root % 1 == 0
-  end
-
 end
--- spec/prime_multiplication_table_spec.rb
+++ spec/prime_multiplication_table_spec.rb
@@ -4,6 +4,29 @@ describe PrimeMultiplicationTable do
   let(:size) { 2 }
   let(:prime_table) { PrimeMultiplicationTable.new(size) }
 
+  describe 'initializing a new PrimeMultiplicationTable' do
+    context 'the table_size is greater than 0' do
+      it 'initializes a new instance of PrimeMultiplicationTable' do
+        expect(prime_table).to be
+      end
+    end
+
+    context 'the table_size is less than 1' do
+      let(:size) { 0 }
+      it 'raises an error' do
+        expect{ prime_table }.to raise_error(RuntimeError)
+      end
+    end
+
+    context 'the a table_size is not explicitly declared' do
+      let(:prime_table) { PrimeMultiplicationTable.new }
+
+      it 'has a default table_size of 10' do
+        expect(prime_table.table_size).to eq 10
+      end
+    end
+  end
+
   describe '#is_prime?' do
     context 'the number is prime' do
       it 'returns true' do
@@ -16,15 +39,17 @@ describe PrimeMultiplicationTable do
     context 'the number is not prime' do
       it 'returns false' do
         expect(prime_table.is_prime?(1)).to be(false)
-        expect(prime_table.is_prime?(4)).to be(false)
         expect(prime_table.is_prime?(9)).to be(false)
+        expect(prime_table.is_prime?(35)).to be(false)
       end
     end
   end
 
   describe '#primes' do
+    subject { prime_table.primes }
+
     it 'returns an array of primes' do
-      expect(prime_table.primes).to eq([2,3])
+      expect(subject).to eq([2,3])
     end
 
     context 'when the prime_table length is changed' do
@@ -32,16 +57,25 @@ describe PrimeMultiplicationTable do
       let(:ten_primes) { [2,3,5,7,11,13,17,19,23,29] }
 
       it 'returns an array of primes of a specified length' do
-        expect(prime_table.primes).to eq(ten_primes)
+        expect(subject).to eq(ten_primes)
       end
     end
   end
 
-  xdescribe '#primes_multiples' do
+  describe '#prime_multiple_table_rows' do
     it "returns a multidimensional array of the primes' multiples" do
-      expect(prime_table.primes_multiples.length).to eq(size)
-      expect(prime_table.primes_multiples[0]).to eq([4,6])
-      expect(prime_table.primes_multiples[1]).to eq([6,9])
+      expect(prime_table.prime_multiple_table_rows.length).to eq(size)
+      expect(prime_table.prime_multiple_table_rows[0]).to eq([2,4,6])
+      expect(prime_table.prime_multiple_table_rows[1]).to eq([3,6,9])
+    end
+  end
+
+  describe '#prime_table_printer' do
+    let(:size) { 10 }
+
+    it 'prints the correct values' do
+      expect(prime_table.prime_table_printer[0]).to eq(prime_table.prime_multiple_table_rows[0])
+      expect(prime_table.prime_table_printer[9]).to eq(prime_table.prime_multiple_table_rows[9])
     end
   end
 end
